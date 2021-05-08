f2ff6278f751feef21168749acf60273992d3e45
Olof Haglund <olof@olofhaglund.name>
2021-05-08 15:11:28 +0200

    Refactored the help messages

--- alice/commands/help_message.py
+++ alice/commands/help_message.py
@@ -1,13 +1,15 @@
 """Methods connected to the /help command"""
+
+from alice.commands import remindme
+
 async def reply(message):
     """Sends private message to the caller"""
-    response = '''
-    Hi, I'm Alice I can do the following:
-    I can set a reminder for you by using the following command:
-    ```
-    /remindme [int][s/m/h]
-    /remindme 10s
-    ```
+    response = f'''
+Hi, I'm Alice I can do the following:
+Remind you after a specific duration
+{remindme.help_string(True)}
+
+{remindme.help_string()}
 '''
 
     await message.author.send(response)
--- alice/commands/remindme.py
+++ alice/commands/remindme.py
@@ -21,4 +21,41 @@ async def reply(message):
     except Exception as e: # pylint: disable=W0703,C0103
         # TODO: respond with a help message
         logging.debug(f"couldn't parse remindme message: {message} e: {e}")
-        await message.reply('I could not parse the value of /remindme')
+        await message.reply(help_string())
+
+
+def help_string(short = False):
+    """Print the help message for /remindme
+
+    Arguments:
+    short - If you want the short string or the long string. Boolean default to False
+"""
+
+    short_string = '`/remindme 1w 2d 3h 4m 5s`'
+
+    if short:
+        return short_string
+
+    return f"""Valid formats for {short_string}
+```
+/remindme 5s
+/remindme 5 sec
+/remindme 5 seconds
+/remindme 3m
+/remindme 3.3m
+/remindme 3 min
+/remindme 1 minute
+/remindme 3 minutes
+/remindme 2h
+/remindme 2hr
+/remindme 2 hours
+/remindme 6d
+/remindme 6 days
+/remindme 1w
+/remindme 1wk
+/remindme 1 week
+/remindme 2 weeks
+
+/remindme 1w 2d 3h 4m 5s
+```
+"""
